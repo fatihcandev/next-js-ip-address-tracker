fec8d4dc4df0c0cf5109fedc296e84f0887dc075
fatihcandev <contact@fatihcan.dev>
2021-05-29 16:21:29 +0300

    add fonts

--- pages/_document.js
+++ pages/_document.js
@@ -31,6 +31,11 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
         <body>
           <Main />
           <NextScript />
