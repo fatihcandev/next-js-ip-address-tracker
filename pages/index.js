59f69a3be9a00b31b3e43284f74dadc7a0934251
fatihcandev <contact@fatihcan.dev>
2021-05-29 19:24:27 +0300

    remove ssr

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,37 @@
-import HomePage from './home'
+import { useEffect, useState } from 'react'
 
 import fetcher from '../utils/fetcher'
 
+import MapComponent from '../components/MapComponent'
+
+import { Main, Background, MapContainer, Title } from '../styles/pages/home'
+
 const Home = ({ geoLocation }) => {
-  return <HomePage geoLocation={geoLocation} />
+  const [position, setPosition] = useState({
+    lat: 0,
+    lng: 0,
+  })
+
+  useEffect(() => {
+    if (geoLocation) {
+      const { latitude: lat, longitude: lng } = geoLocation
+      setPosition({ lat, lng })
+    }
+  }, [geoLocation])
+
+  return (
+    <Main>
+      <Background>
+        <Title>IP Address Tracker</Title>
+      </Background>
+      <MapContainer>
+        <MapComponent position={position} />
+      </MapContainer>
+    </Main>
+  )
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const geoLocation = await fetcher('https://ifconfig.co/json')
 
   return {
